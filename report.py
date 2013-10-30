547ad9af7f82442e97f296c7433c988045091c54
philip-a <philip.arthur.om0@is.naist.jp>
2013-10-30 13:10:58 +0900

    enhance report

--- report.py
+++ report.py
@@ -20,17 +20,23 @@ INDENT = 2
 CHOICE = ['A','B','C','D','E','F','G']
 
 def report(test_data, information,weight):
+	output = [[] for i in range(0, sum([len(x) for x in test_data]))]
 	# prepare to write
 	with prepare_to_write() as f:
 		# loop for each test-data
+		index = 0
 		for i in range(0,len(test_data)):
 			write(f, 'Writing report for test data - ' + str(information[i]))
 			for j in range(0, len(test_data[i])):
 				deep_write(f,1,'Test Doc - [' + str(j+1) + ']')
-				_report(f,test_data[i][j],weight)
+				_report(f,test_data[i][j],weight,output[j])
+				index += 1
+
+		print_output(f,output)
+
 	print >> sys.stderr, 'Finished writing a report to report/report.txt'
 
-def _report(f,test_set,weight):
+def _report(f,test_set,weight,output):
 	doc = test_set['doc']
 
 	deep_write(f,1,'Document')
@@ -42,6 +48,14 @@ def _report(f,test_set,weight):
 		deep_write(f,2, str(i+1) + '. ' + viewable(questions[i]['q_str']))
 		candidates = questions[i]['answer']
 		deep_write(f,2, "System's Answer: " + (str(CHOICE[questions[i]['output']]) if questions[i]['output'] is not None else '-') + " ["+questions[i]['evaluation']+"]")
+
+		if questions[i]['evaluation'] == 'Correct':
+			output.append('_')
+		elif questions[i]['evaluation'] == 'Incorrect':
+			output.append(str(CHOICE[questions[i]['output']]))
+		else: 
+			output.append('?')
+
 		for j in range(0,len(candidates)):
 			deep_write(f,3, is_correct(CHOICE[j], candidates[j]) + '. ' + viewable(candidates[j]['value']))
 			features = candidates[j]['score']
@@ -51,6 +65,23 @@ def _report(f,test_set,weight):
 			deep_write(f,4,"-" * 60 + '+')
 			deep_write(f,4,"Total Score: " + str(candidates[j]['total_score']))
 		write(f)
+
+def print_output(f, output):
+	max_len = max([len(x) for x in output])
+
+	# header
+	f.write('  ')
+	for i in range(0,max_len):
+		f.write(' ' + "%02d" %(i+1))
+	f.write('\n')  
+
+	# body
+	for i in range(0,len(output)):
+		f.write(("%02d" % (i+1)))
+		for j in range (0, len(output[i])):
+			f.write('  ' + output[i][j])
+		f.write('\n')
+	f.flush()
 
 def is_correct(letter, candidate):
 	if "correct" in candidate and candidate["correct"]:
